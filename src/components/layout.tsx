9b58d721f42102d22aec84056ab8c2ec56d76a68
Sepide Kia <sepidekia6990@gmail.com>
2023-10-24 19:00:08 +0330

    create the projects page

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,7 +5,10 @@ type NavItemProps = {
 };
 function NavItem({ title, url, isSelected }: NavItemProps) {
   return (
-    <li>
+    <li
+      className="rounded-lg bg-white/90 px-3 text-sm fontmedium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5
+    backdrop-blur"
+    >
       <a
         className={`block px-3 py-2 transition hover:text-teal-500 ${
           isSelected ? 'text-teal-500' : ''
@@ -25,10 +28,7 @@ function Navbar({ pageId }: NavbarProps) {
   return (
     <div className="flex justify-center mx-auto max-w-7xl h-16 pt-6">
       <nav>
-        <ul
-          className="flex rounded-full bg-white/90 px-3 text-sm fontmedium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5
-backdrop-blur"
-        >
+        <ul className="flex gap-1 ">
           <NavItem
             title="About Me"
             url="/"
@@ -54,6 +54,37 @@ export default function Layout({ children }: any) {
     <>
       <Navbar pageId={children.props.pageId} />
       <main>{children}</main>
+      <Footer />
     </>
+  );
+}
+
+type FooterLinkProps = {
+  text: string;
+  url: string;
+};
+
+function FooterLink({ url, text }: FooterLinkProps) {
+  return (
+    <a className="transition hover:text-teal-500" href={url}>
+      {text}
+    </a>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="pt-10 px-8 pb-16 border-t">
+      <div className="flex justify-between gap-6">
+        <div className="flex gap-6 text-sm font-medium text-zinc-600">
+          <FooterLink text={'About Me'} url={'/'} />
+          <FooterLink text={'Projects'} url={'/projects'} />
+          <FooterLink text={'Essays'} url={'/essays'} />
+        </div>
+        <p className="text-sm text-zinc-400">
+          © 2023 Grace Huang. All rights reserved.
+        </p>
+      </div>
+    </footer>
   );
 }
